f4009ec377ffbccffbbb81c3d4213e626ecb3437
AlanNgaiii <634620668@qq.com>
2022-09-13 15:37:37 +0800

    记录单行虚线

--- src/pages/hearth/custom_border/index.vue
+++ src/pages/hearth/custom_border/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="custom-border">
         <div class="border-div"></div>
+        <div class="dash-line"></div>
     </div>
 </template>
 
@@ -13,7 +14,16 @@
         border: 1px dashed transparent;
         background: linear-gradient(white, white) padding-box,
             repeating-linear-gradient(-45deg, #b6b6b6 0, #b6b6b6 0.25em, white 0, white 0.75em);
-            // 调整 0.25 和 0.75 到想要的效果
+        // 调整 0.25 和 0.75 到想要的效果
+    }
+
+    .dash-line {
+        flex: 1;
+        height: 3px;
+        background-image: linear-gradient(to right, #e2e2e2 0%, #e2e2e2 50%, transparent 50%);// 调整下 50% 看到间隙
+        background-size: 20px 3px;// 调整 20px 看到间隙
+        background-repeat: repeat-x;
+        margin-top: 100px;
     }
 }
 </style>
